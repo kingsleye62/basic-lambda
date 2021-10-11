ef84e1e0f75b8541fa5855915a891b64120f25e6
kingsleye62 <kingsleye62@gmail.com>
2021-10-11 13:43:28 -0400

    test

--- backend.tf
+++ backend.tf
@@ -15,6 +15,7 @@ terraform {
    encrypt        = true
    kms_key_id     = "alias/terraform-bucket-key"
    dynamodb_table = "terraform-state"
+   role_arn = "arn:aws:iam::409592171686:user/terraform-user"
  }
 }
 
