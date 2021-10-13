ef7b56174e2ac9e93e989c3faa0dd947bf6e638b
kingsleye62 <kingsleye62@gmail.com>
2021-10-13 09:51:37 -0400

    test

--- backend.tf
+++ backend.tf
@@ -7,20 +7,20 @@ data "aws_dynamodb_table" "dynamodb_state" {
 }
 
 
-# terraform {
-#  backend "s3" {
-#    bucket         = "king-aws-cicd-pipiline"
-#    key            = "state/terraform.tfstate"
-#    region         = "us-east-1"
-#    encrypt        = true
-#    kms_key_id     = "alias/terraform-bucket-key"
-#    dynamodb_table = "terraform-state"
-#    role_arn = "arn:aws:iam::409592171686:role/EC2-Admin"
-#    assume_role_tags = {
-#      Name = "value"
-#     }
-#  }
-# }
+terraform {
+ backend "s3" {
+   bucket         = "king-aws-cicd-pipiline"
+   key            = "state/terraform.tfstate"
+   region         = "us-east-1"
+   encrypt        = true
+   kms_key_id     = "alias/terraform-bucket-key"
+   dynamodb_table = "terraform-state"
+   role_arn = "arn:aws:iam::409592171686:role/EC2-Admin"
+   assume_role_tags = {
+     Name = "value"
+    }
+ }
+}
 
 output "get_aws_s3_bucket" {
   value = data.aws_s3_bucket.s3_bucket_state.bucket
